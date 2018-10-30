047f0e81814c1612c3a70d114b6f1436b37511a4
Iris The Fox <iris-miz@yandex.ru>
2018-10-30 12:28:43 +0300

    add authorization like inlessons

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
- # resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'firearms#index'
   resources :firearms
   get "firearms" => 'firearms#index'
 
   resources :choosen_arms, only: [:new, :create, :show]
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
 
 #  get "/login" => "sessions#new", as: "login"
  # delete "/logout" => "sessions#destroy", as: "logout"
