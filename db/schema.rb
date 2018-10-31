d04be61f3a08b01eab7a3cad0a67f8fb65963a47
Iris The Fox <iris-miz@yandex.ru>
2018-10-31 11:28:43 +0300

    add news

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_090441) do
+ActiveRecord::Schema.define(version: 2018_10_31_081403) do
 
   create_table "choosen_arms", force: :cascade do |t|
     t.string "firearm_id"
@@ -37,6 +37,15 @@ ActiveRecord::Schema.define(version: 2018_10_30_090441) do
     t.string "fr_mat"
     t.string "stock_type"
     t.string "img_main"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.date "date"
+    t.integer "range"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
