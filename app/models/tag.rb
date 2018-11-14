10c4e2783b3295b2e9f585692170b6a2e08953df
Iris The Fox <iris-miz@yandex.ru>
2018-11-14 13:57:19 +0300

    little changes in tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,7 @@
 class Tag < ApplicationRecord
 	has_and_belongs_to_many :posts
+	
+	def self.counts
+      self.select("name, count(posts_tags.tag_id) as count").joins(:posts_tags).group("name, posts_tags.tag_id")
+    end
 end
