9586faf23107f205b07881e20798cff405d3de8b
Iris The Fox <iris-miz@yandex.ru>
2018-11-21 17:02:19 +0300

    delete pragma lines from add summary

--- db/migrate/20181120094035_add_summary_to_posts.rb
+++ db/migrate/20181120094035_add_summary_to_posts.rb
@@ -1,11 +1,9 @@
 class AddSummaryToPosts < ActiveRecord::Migration[5.2]
   def change
-    connection.execute("PRAGMA defer_foreign_keys = ON")
-    connection.execute("PRAGMA foreign_keys = OFF")
+
     add_column :posts, :summary, :string
     change_column :posts, :range, :integer, {default:0}
-    connection.execute("PRAGMA foreign_keys = ON")
-    connection.execute("PRAGMA defer_foreign_keys = OFF")
+
     
   end
 end
