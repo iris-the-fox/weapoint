e200900430a4b4addeded50293ddcf5109f245b2
Iris The Fox <iris-miz@yandex.ru>
2018-12-05 12:29:33 +0300

    remove pragma from migrations

--- db/migrate/20181126111228_remove_img_from_firearms.rb
+++ db/migrate/20181126111228_remove_img_from_firearms.rb
@@ -1,10 +1,8 @@
 class RemoveImgFromFirearms < ActiveRecord::Migration[5.2]
   def change
-  	connection.execute("PRAGMA defer_foreign_keys = ON")
-connection.execute("PRAGMA foreign_keys = OFF")
+
 remove_column :firearms, :img_main
-connection.execute("PRAGMA foreign_keys = ON")
-connection.execute("PRAGMA defer_foreign_keys = OFF")
+
     
   end
 end
