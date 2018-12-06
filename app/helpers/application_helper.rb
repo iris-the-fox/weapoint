8158de222c9d8a236c7567c8ddce8e1ee030b357
Iris The Fox <iris-miz@yandex.ru>
2018-12-06 15:16:35 +0300

    add multiple form for ammunition

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-	def current_class?(test_path)
+  def current_class?(test_path)
     request.path == test_path ? 'active' : ''
+  end
+
+   def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
   end
 end
