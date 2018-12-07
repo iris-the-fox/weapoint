29ffbfb0b6d134a4a66964cf15aa775de847026a
Iris The Fox <iris-miz@yandex.ru>
2018-12-07 15:37:42 +0300

    add reject if

--- app/models/ammunition.rb
+++ app/models/ammunition.rb
@@ -1,4 +1,4 @@
 class Ammunition < ApplicationRecord
 	has_many :pictures, as: :imageable, dependent: :destroy
-	accepts_nested_attributes_for :pictures
+	accepts_nested_attributes_for :pictures, reject_if: :all_blank
 end
