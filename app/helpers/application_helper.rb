1bbe31ca0633c76ffb80595d147ddfcf2cd840f4
Iris The Fox <iris-miz@yandex.ru>
2018-12-07 15:09:35 +0300

    delete presence validation for pictures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,5 @@ module ApplicationHelper
     request.path == test_path ? 'active' : ''
   end
 
-   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
-  end
+
 end
