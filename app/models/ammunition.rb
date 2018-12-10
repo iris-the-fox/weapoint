fb1c973724d3b139d8e7cbc555e194c5d7eee397
Iris The Fox <iris-miz@yandex.ru>
2018-12-10 16:44:24 +0300

    fix bug dont saving notes for ammunitions

--- app/models/ammunition.rb
+++ app/models/ammunition.rb
@@ -1,4 +1,5 @@
 class Ammunition < ApplicationRecord
 	has_many :pictures, as: :imageable, dependent: :destroy
 	accepts_nested_attributes_for :pictures, reject_if: :all_blank
+	validates  :note, presence: true
 end
