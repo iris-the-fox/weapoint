ced41a467ffefb47cc7500a316e97dc929b8b14c
Iris The Fox <iris-miz@yandex.ru>
2019-01-15 12:49:21 +0300

    add to search select tag helper

--- app/helpers/ammunitions_helper.rb
+++ app/helpers/ammunitions_helper.rb
@@ -1,2 +1,8 @@
 module AmmunitionsHelper
+	def select_by_field (parametr_for, prompt_for)
+
+		select_tag( parametr_for, options_for_select(Ammunition.all.map {|am| am.send(parametr_for)}.compact.uniq.sort),  prompt: prompt_for, class: "form_class")
+	end
+
+
 end
